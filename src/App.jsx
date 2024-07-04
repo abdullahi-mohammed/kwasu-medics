ed980d076c822b4fc7cd21a04de3c386b71280db
abdullahi <moh.abdullahi2003@gmail.com>
2024-07-04 12:39:24 +0100

    initializing project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import Login from './pages/login/Login'
+
+function App() {
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/login"} exact element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
