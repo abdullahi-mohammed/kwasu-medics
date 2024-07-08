45419275a1a5306f66bd27bee0f4e010fd14ac1f
Pacifire <69111834+adedyife@users.noreply.github.com>
2024-07-08 17:25:19 +0100

    form toggle

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,12 +1,17 @@
-import React from 'react'
-import Logos from '../../components/login/logos/logos'
+import React from "react";
+import Logos from "../../components/login/logos/logos";
+import styles from "./login.module.css";
+import Form from "../../components/login/form/form";
+import FormToggle from "../../components/login/formToggle/formToggle";
 
 const Login = () => {
-    return (
-        <div>
-            <Logos />
-        </div>
-    )
-}
+  return (
+    <div className={styles.login}>
+      <Logos />
+      <FormToggle />
+      <Form />
+    </div>
+  );
+};
 
-export default Login
+export default Login;
