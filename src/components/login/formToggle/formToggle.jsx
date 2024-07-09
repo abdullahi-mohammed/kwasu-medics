aa07c122a7ba02e0316df796d202f3f2ff91794a
abdullahi <moh.abdullahi2003@gmail.com>
2024-07-09 19:53:32 +0100

    login page updated

--- src/components/login/formToggle/formToggle.jsx
+++ src/components/login/formToggle/formToggle.jsx
@@ -1,22 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./formToggle.module.css";
 
 function FormToggle() {
+  const [user, setUser] = useState("student")
   let position = "";
 
   const clickStuHandler = (e) => {
     position = "Student";
+    setUser("student")
     console.log(position);
   };
   const clickDocHandler = (e) => {
+    setUser("doctor")
     position = "Doctor";
     console.log(position);
   };
 
   return (
     <div className={styles.formToggle}>
-      <div onClick={() => clickStuHandler(e)}>Student</div>
-      <div onClick={() => clickDocHandler(e)}>Doctor</div>
+      <div onClick={clickStuHandler} className={user == "student" ? styles.studentActive : ""}>Student</div>
+      <div onClick={clickDocHandler} className={user == "doctor" ? styles.doctorActive : ""}>Doctor</div>
     </div>
   );
 }
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,19 +1,43 @@
 import React, { useState } from "react";
 import Logos from "../../components/login/logos/logos";
 import styles from "./login.module.css";
-
-import FormToggle from "../../components/login/formToggle/formToggle";
-import Stuform from "../../components/login/form/stuForm/stuForm";
-import DocForm from "../../components/login/form/docForm/docForm";
+import { getImageUrl } from "../../helpers/imageHelper";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  // const [position, setPosition] = useState("dd");
+  const [user, setUser] = useState("student")
 
+  const clickStuHandler = (e) => {
+    setUser("student")
+  };
+
+  const clickDocHandler = (e) => {
+    setUser("doctor")
+  };
   return (
-    <div className={styles.login}>
-      <Logos />
-      <FormToggle />
-      <DocForm />
+    <div className={styles.wrapper}>
+      <div className={styles.login}>
+        <div className={styles.centered}>
+          <Logos />
+          <div className={styles.formToggle}>
+            <div onClick={clickStuHandler} className={user == "student" ? styles.studentActive : ""}>Student</div>
+            <div onClick={clickDocHandler} className={user == "doctor" ? styles.doctorActive : ""}>Doctor</div>
+          </div>
+          <form className={styles.stuForm}>
+            <h2>Login</h2>
+            <div className="space-y-4 mt-3">
+              <input type="text" placeholder="Username/Reg No./Matric No." autoComplete="true" />
+              <input type="password" placeholder="Password" autoComplete="true" />
+            </div>
+            <hr className="w-[100%] bg-[#7f8fd5] border-[#7f8fd5] my-10" />
+            <div className="flex items-center gap-2 mb-3 ">
+              <h3>Forgot Password? </h3>
+              <Link to="/forgot-password">Reset Password</Link>
+            </div>
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
